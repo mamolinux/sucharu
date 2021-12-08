b283c0d8be181e6f035ff9e61058d03b62055022
Himadri Sekhar Basu <hsb10@iitbbs.ac.in>
2021-12-08 11:30:06 +0530

    Use multiple processors to generate assets faster

--- src/Mint-Y/gtk-3.0/render-assets.sh
+++ src/Mint-Y/gtk-3.0/render-assets.sh
@@ -9,28 +9,48 @@ INDEX="assets.txt"
 
 ulimit -s 1024
 
+render()
+{
+    echo Rendering $1
+    if [[ "$1" == *"@2"* ]]; then
+        $INKSCAPE --export-id=$2 \
+                --export-dpi=192 \
+                --export-id-only \
+                --export-png=$1 $SRC_FILE >/dev/null \
+        && $OPTIPNG -o7 --quiet $1
+    else
+        $INKSCAPE --export-id=$2 \
+                --export-id-only \
+                --export-png=$1 $SRC_FILE >/dev/null \
+        && $OPTIPNG -o7 --quiet $1
+    fi
+}
+
 for i in `cat $INDEX`
 do 
 if [ -f $ASSETS_DIR/$i.png ]; then
     echo $ASSETS_DIR/$i.png exists.
 else
     echo
-    echo Rendering $ASSETS_DIR/$i.png
-    $INKSCAPE --export-id=$i \
-              --export-id-only \
-              --export-png=$ASSETS_DIR/$i.png $SRC_FILE >/dev/null \
-    && $OPTIPNG -o7 --quiet $ASSETS_DIR/$i.png 
+    render $ASSETS_DIR/$i.png $i &
+    # allow only to execute number of jobs in parallel
+    # equal to number of processors
+    if [[ $(jobs -r -p | wc -l) -gt $(nproc) ]]; then
+    # wait only for first job
+    wait $(jobs -p)
+    fi
 fi
 if [ -f $ASSETS_DIR/$i@2.png ]; then
     echo $ASSETS_DIR/$i@2.png exists.
 else
     echo
-    echo Rendering $ASSETS_DIR/$i@2.png
-    $INKSCAPE --export-id=$i \
-              --export-dpi=192 \
-              --export-id-only \
-              --export-png=$ASSETS_DIR/$i@2.png $SRC_FILE >/dev/null \
-    && $OPTIPNG -o7 --quiet $ASSETS_DIR/$i@2.png 
+    render $ASSETS_DIR/$i@2.png $i &
+    # allow only to execute number of jobs in parallel
+    # equal to number of processors
+    if [[ $(jobs -r -p | wc -l) -gt $(nproc) ]]; then
+    # wait only for first job
+    wait $(jobs -p)
+    fi
 fi
 done
 exit 0
